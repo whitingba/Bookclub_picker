31e903f564af501f54b92d953489729686d00a1b
Barbara Whiting <whitingba@gmail.com>
2021-02-06 11:56:22 -0700

    added forEach loop to loop through the array of names

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var nameArray = [];
 
 function submitName() {
   let name1 = document.getElementById('FirstName').value;
-  console.log(name1);
+  //console.log(name1);
 
 
   nameArray.push(name1);
@@ -15,14 +15,15 @@ function submitName() {
 
 function addNames() {
 
-
+  //grab an id in the HTML and create a variable for a span
   let newSpace = document.getElementById('desert');
   let newSpan = document.createElement('span');
-  newSpan.innerHTML = nameArray[0];
 
+  //loop through the names give and insert them in to the HTML on their own line
+  nameArray.forEach(function (eachName) {
+    newSpan.innerHTML = eachName + "</br>";
+  })
   newSpace.appendChild(newSpan);
-
-  console.log(nameArray);
 
 }
 
