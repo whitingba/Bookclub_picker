1b90766273e5b567436e04bbd370ffe7aa83ea4b
Barbara Whiting <whitingba@gmail.com>
2021-02-06 08:03:35 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+
+var nameArray = [];
+
+function submitName() {
+  let name1 = document.getElementById('FirstName').value;
+  console.log(name1);
+
+
+  nameArray.push(name1);
+  console.log(nameArray);
+}
+
+
+
