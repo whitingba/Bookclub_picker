cb786c57f818cf9a1009294adf39012f802c3cb7
Barbara Whiting <whitingba@gmail.com>
2021-02-06 12:04:39 -0700

    added button for use to select a random bookclub host

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 
-
-
 var nameArray = [];
+let newSpace = document.getElementById('desert');
+
 
 function submitName() {
   let name1 = document.getElementById('FirstName').value;
@@ -16,7 +16,7 @@ function submitName() {
 function addNames() {
 
   //grab an id in the HTML and create a variable for a span
-  let newSpace = document.getElementById('desert');
+
   let newSpan = document.createElement('span');
 
   //loop through the names give and insert them in to the HTML on their own line
@@ -24,7 +24,19 @@ function addNames() {
     newSpan.innerHTML = eachName + "</br>";
   })
   newSpace.appendChild(newSpan);
+  addButton();
+}
+//add Randomizer button if there is more than one name in the array
+function addButton() {
 
+  if (nameArray.length >= 2) {
+    //console.log('there are 2 or more items in the array');
+    let newButton = document.createElement('button');
+    newButton.className = 'btn';
+    newButton.innerHTML = "Pick Random host";
+    newSpace.appendChild(newButton);
+
+  }
 }
 
 
