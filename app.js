804adc4bc02f6ec4e8ec0610b9759c200b0ef9b8
Barbara Whiting <whitingba@gmail.com>
2021-02-06 13:38:58 -0700

    dynamically show random button and test for button onclick event

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 
 var nameArray = [];
-let newSpace = document.getElementById('desert');
+let hostNameDiv = document.getElementById('desert');
+let secondSpace = document.getElementById('desert2');
+let thirdSpace = document.getElementById('desert3');
 
 
 function submitName() {
@@ -13,30 +15,60 @@ function submitName() {
   addNames();
 }
 
+//function to add names to the div with id 'desert'
 function addNames() {
 
   //grab an id in the HTML and create a variable for a span
-
   let newSpan = document.createElement('span');
 
   //loop through the names give and insert them in to the HTML on their own line
   nameArray.forEach(function (eachName) {
     newSpan.innerHTML = eachName + "</br>";
   })
-  newSpace.appendChild(newSpan);
+  hostNameDiv.appendChild(newSpan);
+
   addButton();
 }
+
+
 //add Randomizer button if there is more than one name in the array
 function addButton() {
+  let randomButton = document.getElementById('randomizerBtn');
 
   if (nameArray.length >= 2) {
     //console.log('there are 2 or more items in the array');
-    let newButton = document.createElement('button');
-    newButton.className = 'btn';
-    newButton.innerHTML = "Pick Random host";
-    newSpace.appendChild(newButton);
+    // let newButton = document.createElement('button');
+    // newButton.className = 'btn';
+    // newButton.id = 'randomizerBtn';
+    // newButton.innerHTML = "Pick Random host";
+    // hostNameDiv.appendChild(newButton);
+    secondSpace.appendChild(randomButton);
+    randomButton.style.display = "block";
 
+    // randomButton.addEventListener('click', selectRandom());
+
+
+  } else {
+    randomButton.style.display = "none";
   }
+}
+
+
+// if (randomButton) {
+//   randomButton.addEventListener('click', function () {
+//     console.log("button clicked");
+//   });
+// };
+
+
+
+
+//select a random bookclub host based on the selections available in the array
+function selectRandom() {
+  //select random name from Array
+  let randomName = nameArray[Math.floor(Math.random() * nameArray.length)];
+  console.log('Random name: ' + randomName);
+
 }
 
 
